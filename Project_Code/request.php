8b59673af7fc8cebc6b4b2df1c15c963d257e0c3
herasymj <jennifer.herasymuik@gmail.com>
2018-03-24 20:08:33 -0600

    Added all php files we will need
    Not finished, missing the login php and all files need to beedited still

--- Project_Code/request.php
+++ Project_Code/request.php
@@ -1,3 +1,31 @@
 <?php
 
+class Request {
+	//members
+	var $requestID;
+	var $userID;
+	var $approverID;
+	var $analystID;
+	var $name;
+	var $email;
+	var $contactNum;
+	var $department;
+	
+	var $appName;
+	var $permissionType;
+	var $reason;
+	var $approver;
+	
+	var $analystApproved;
+	var $approverApproved;
+	var $permissionGranted;
+	var $isOpen;
+	
+  
+	//functions
+	function add_value ($input_param) {
+		return include ("path/some_file.php");
+	}
+}
+
 ?>
