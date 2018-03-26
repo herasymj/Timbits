c4173df51e69a701918406bc3a30afe0ddf0b204
BuildTools <unconfigured@null.spigotmc.org>
2018-03-25 21:54:20 -0600

    Worked on php files for logging in/signing up and finished the global session file

--- Project_Code/dbConfig.php
+++ Project_Code/dbConfig.php
@@ -4,4 +4,5 @@
    define('DB_PASSWORD', 'rootpassword');//change this in server!!!!
    define('DB_DATABASE', 'HELLs App Permissions');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
 ?>
--- Project_Code/session.php
+++ Project_Code/session.php
@@ -1,16 +1,17 @@
 <?php
    include('dbConfig.php');
    session_start();
+   //from https://www.tutorialspoint.com/php/php_mysql_login.htm
    //edit this to work with our db
-   $user_check = $_SESSION['login_user'];
+   $user_email = $_SESSION['user'];
    
-   $ses_sql = mysqli_query($db,"select username from admin where username = '$user_check' ");
+   $get_user = mysqli_query($db,"select * from Users where EMAIL = '$user_email' ");
    
-   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+   $row = mysqli_fetch_array($get_user,MYSQLI_ASSOC);
    
-   $login_session = $row['username'];
+   $user = new user($row['USER_ID'], $row['FIRST_NAME'], $row['LAST_NAME'], $row['EMAIL'], $row['IS_APPROVER'], $row['IS_ANALYST']);
    
-   if(!isset($_SESSION['login_user'])){
-      header("location:login.php");
+   if(!isset($_SESSION['user'])){
+      header("location:index.php");
    }
 ?>
